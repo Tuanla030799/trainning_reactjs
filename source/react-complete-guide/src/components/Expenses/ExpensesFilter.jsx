83487be6b2f369967c59916f4d9640aa151034e1
tuanla-2957 <le.anh.tuan-b@sun-asterisk.com>
2022-09-14 16:46:58 +0700

    feat: section4: UI

---
+++ source/react-complete-guide/src/components/Expenses/ExpensesFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default ExpensesFilter; props
